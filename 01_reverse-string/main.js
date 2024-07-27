9606137d51e8ddae8c0dfa819d9d0eacb2f77d9e
Jan Gross <jan.gross.cze@gmail.com>
2024-07-27 14:51:09 +0200

    done

--- 01_reverse-string/main.js
+++ 01_reverse-string/main.js
@@ -1,24 +1,20 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const str1 = "ahoj"
+const str2 = "hello"
+const str3 = "CodingMoney"
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const number1 = 15
+const number2 = -456
+const number3 = 189
 
-setupCounter(document.querySelector('#counter'))
+const reverse = (str) => {
+  const rev = str.split("").reverse().join("")
+  console.log(rev)
+}
+reverse(str1)
+
+const reverseNum = (n) => {
+  const reversed = n.toString().split('').reverse().join('')
+  return parseInt(reversed) * Math.sign(n)
+}
+console.log(reverseNum(number1))
+  
